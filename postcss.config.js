d7c978c7e981297820ee20d437d0b5508de4d24e
Nico Prat <nicooprat@gmail.com>
2020-05-04 15:49:33 +0200

    Update Tailwind and use its own PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,17 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-
-  // Specify the paths to all of the template files in your project
-  content: [
-    './src/**/*.html',
-    './src/**/*.vue',
-    './src/**/*.jsx',
-    // etc.
-  ],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-})
-
 module.exports = {
   plugins: [
-    require('tailwindcss'),
+    require('tailwindcss')({
+      purge: [
+        './src/**/*.html',
+        './src/**/*.vue',
+      ],
+      corePlugins: {
+        container: false,
+      },
+      theme: {},
+      variants: {},
+      plugins: [],
+    }),
     require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production'
-      ? [purgecss]
-      : []
   ]
 }
